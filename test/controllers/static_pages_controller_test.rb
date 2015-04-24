25538a1fc28da58abe393f74cf0048620369fc7f
Milan Darjanin <me@milandarjanin.com>
2015-04-24 15:44:03 +0200

    before deploy

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Ruby"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Ruby | Help"
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "Ruby | About"
   end
 
 end
